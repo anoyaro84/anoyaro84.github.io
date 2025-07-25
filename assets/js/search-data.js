af8d88713013c77dcd4a60c7429e09d6a047e6f2
anoyaro84 <anoyaro84@gmail.com>
2025-07-25 09:07:34 +0000

    Deploying to gh-pages from @ anoyaro84/anoyaro84.github.io@0387cf0e9daf71967e2f0d436c1ddf783f31d195 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Below you can find the details of my profile. PDf version is also available (see right).",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
